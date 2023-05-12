d0c4c30b501ca0791c765d1a61aa5cd012f44e06
velsanjay <sanjaymech2313@mail.com>
2023-05-12 18:15:43 +0530

    check

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ require("./Common/dbconfig")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jade'); 
 
 app.use(cors())
 app.use(logger('dev'));
